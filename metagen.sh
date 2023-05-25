eb0f78dc5946d2486f1ea2e4b02385e6511442cf
guaiguastalinbiotech <stalin.guaigua@est.ikiam.edu.ec>
2023-05-25 08:53:31 -0500

    Terminamos la pregunta 5

---
+++ metagen.sh
@@ -0,0 +1,13 @@
+# Pregunta #5 
+
+egrep "s__" *.txt | wc -l 
+
+
+echo "Resultado de TATA" > gata.txt 
+egrep -o "\w*TATA\w*" mygenomemap.sam >> gata.txt
+
+echo "Resultado de GAGA" > gata.txt 
+egrep -o "\w*GAGA\w*" mygenomemap.sam >> gata.txt
+
+echo "Resultado de GATA" > gata.txt 
+egrep -o "\w*GATA\w*" mygenomemap.sam >> gata.txt
