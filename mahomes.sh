de38605b3c6b37cd8b36cdb9a249e962c7a95bd6
guaiguastalinbiotech <stalin.guaigua@est.ikiam.edu.ec>
2023-05-25 09:11:29 -0500

    Actualizamos la pregunta 4

--- mahomes.sh
+++ mahomes.sh
@@ -2,6 +2,6 @@
 
 cut -d ","  -f5,18,21 sites.csv > pdb.csv 
 
-cut -d "," -f1 pdb.csv | tail -n +2 | sort -u | wc -l 
-cut -d "," -f2 pdb.csv | tail -n +2 | sort -u | wc -l
-cut -d "," -f3 pdb.csv | tail -n +2 | sort -u | wc -l
+cut -d "," -f1 pdb.csv | tail -n +2 | sort -u | wc -l > pdb_count.csv 
+cut -d "," -f2 pdb.csv | tail -n +2 | sort -u | wc -l >> pdb_count.csv
+cut -d "," -f3 pdb.csv | tail -n +2 | sort -u | wc -l >> pdb_count.csv
